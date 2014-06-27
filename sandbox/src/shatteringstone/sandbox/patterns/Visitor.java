e5c3e67c423f81c76ffffd10587016b804a0631c
Bradley, Mike <Mike.Bradley@fpx.com>
2014-06-27 15:56:40 -0500

    Added annotations

--- sandbox/src/shatteringstone/sandbox/patterns/Visitor.java
+++ sandbox/src/shatteringstone/sandbox/patterns/Visitor.java
@@ -2,27 +2,31 @@ package shatteringstone.sandbox.patterns;
 
 interface CarElementVisitor {
     void visit(Wheel wheel);
+
     void visit(Engine engine);
+
     void visit(Body body);
+
     void visit(Car car);
 }
- 
+
 interface CarElement {
     void accept(CarElementVisitor visitor);
 }
- 
+
 class Wheel implements CarElement {
-    private String name;
- 
-    public Wheel(String name) {
+    private final String name;
+
+    public Wheel(final String name) {
         this.name = name;
     }
- 
+
     public String getName() {
         return this.name;
     }
- 
-    public void accept(CarElementVisitor visitor) {
+
+    @Override
+    public void accept(final CarElementVisitor visitor) {
         /*
          * accept(CarElementVisitor) in Wheel implements
          * accept(CarElementVisitor) in CarElement, so the call
@@ -34,82 +38,92 @@ class Wheel implements CarElement {
          * CarElementVisitor implements the visit(Wheel), which is
          * another decision that is made at run time. This can be
          * considered the second dispatch.
-         */ 
+         */
         visitor.visit(this);
     }
 }
- 
+
 class Engine implements CarElement {
-    public void accept(CarElementVisitor visitor) {
+    @Override
+    public void accept(final CarElementVisitor visitor) {
         visitor.visit(this);
     }
 }
- 
+
 class Body implements CarElement {
-    public void accept(CarElementVisitor visitor) {
+    @Override
+    public void accept(final CarElementVisitor visitor) {
         visitor.visit(this);
     }
 }
- 
+
 class Car implements CarElement {
     CarElement[] elements;
- 
+
     public Car() {
         //create new Array of elements
-        this.elements = new CarElement[] { new Wheel("front left"), 
-            new Wheel("front right"), new Wheel("back left") , 
-            new Wheel("back right"), new Body(), new Engine() };
+        this.elements = new CarElement[] { new Wheel("front left"), new Wheel("front right"), new Wheel("back left"),
+                new Wheel("back right"), new Body(), new Engine() };
     }
- 
-    public void accept(CarElementVisitor visitor) {     
-        for(CarElement elem : elements) {
+
+    @Override
+    public void accept(final CarElementVisitor visitor) {
+        for (CarElement elem : this.elements) {
             elem.accept(visitor);
         }
-        visitor.visit(this);    
+        visitor.visit(this);
     }
 }
- 
+
 class CarElementPrintVisitor implements CarElementVisitor {
-    public void visit(Wheel wheel) {      
+    @Override
+    public void visit(final Wheel wheel) {
         System.out.println("Visiting " + wheel.getName() + " wheel");
     }
- 
-    public void visit(Engine engine) {
+
+    @Override
+    public void visit(final Engine engine) {
         System.out.println("Visiting engine");
     }
- 
-    public void visit(Body body) {
+
+    @Override
+    public void visit(final Body body) {
         System.out.println("Visiting body");
     }
- 
-    public void visit(Car car) {      
+
+    @Override
+    public void visit(final Car car) {
         System.out.println("Visiting car");
     }
 }
- 
+
 class CarElementDoVisitor implements CarElementVisitor {
-    public void visit(Wheel wheel) {
+    @Override
+    public void visit(final Wheel wheel) {
         System.out.println("Kicking my " + wheel.getName() + " wheel");
     }
- 
-    public void visit(Engine engine) {
+
+    @Override
+    public void visit(final Engine engine) {
         System.out.println("Starting my engine");
     }
- 
-    public void visit(Body body) {
+
+    @Override
+    public void visit(final Body body) {
         System.out.println("Moving my body");
     }
- 
-    public void visit(Car car) {
+
+    @Override
+    public void visit(final Car car) {
         System.out.println("Starting my car");
     }
 }
- 
+
 public class Visitor {
-    static public void main(String[] args) {
+    static public void main(final String[] args) {
         Car car = new Car();
         CarElementPrintVisitor visitor = new CarElementPrintVisitor();
-		car.accept(visitor);
+        car.accept(visitor);
         car.accept(new CarElementDoVisitor());
     }
 }
