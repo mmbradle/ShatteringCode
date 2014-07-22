e0b2654e94fc365961eebea3226230f67aebd284
Bradley, Mike <Mike.Bradley@fpx.com>
2014-07-22 11:18:31 -0500

    Updated Bloch's Builder pattern

--- sandbox/src/shatteringstone/sandbox/patterns/Builder_Bloch.java
+++ sandbox/src/shatteringstone/sandbox/patterns/Builder_Bloch.java
@@ -3,61 +3,77 @@ package shatteringstone.sandbox.patterns;
 import shatteringstone.sandbox.patterns.NutritionFacts.NutritionFactsBuilder;
 
 class NutritionFacts {
-	private final int servingSize;
-	private final int servings;
-	private final int calories;
-	private final int fat;
-	private final int sodium;
-	private final int carbs;
-	
-	@Override
-	public String toString() {
-		return "NutritionFacts [servingSize=" + servingSize + ", servings="
-				+ servings + ", calories=" + calories + ", fat=" + fat
-				+ ", sodium=" + sodium + ", carbs=" + carbs + "]";
-	}
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbs;
 
-	/** Use this style of builder to improve telescoping parameters on constructor */
-	public static class NutritionFactsBuilder {
-		// Required parameters
-		private int servingSize;
-		private int servings;
-		
-		// Optional parameters
-		private int calories = 0;
-		private int fat = 0;
-		private int sodium = 0;
-		private int carbs = 0;
-		
-		public NutritionFactsBuilder ( int servingSize, int servings) {
-			this.servingSize = servingSize;
-			this.servings = servings;
-		}
-		
-		public NutritionFactsBuilder calories(int val) { calories = val; return this; }
-		public NutritionFactsBuilder fat(int val) { fat = val; return this; }
-		public NutritionFactsBuilder sodium(int val) { sodium = val; return this; }
-		public NutritionFactsBuilder carbs(int val) { carbs = val; return this; }
-		
-		public NutritionFacts build() {
-			return new NutritionFacts(this);
-		}
-	}
-	
-	private NutritionFacts(NutritionFactsBuilder builder) {
-		servingSize = builder.servingSize;
-		servings = builder.servings;
-		calories = builder.calories;
-		fat = builder.fat;
-		sodium = builder.sodium;
-		carbs = builder.carbs;
-	}
+    @Override
+    public String toString() {
+        return "NutritionFacts [servingSize=" + this.servingSize + ", servings=" + this.servings + ", calories="
+                + this.calories + ", fat=" + this.fat + ", sodium=" + this.sodium + ", carbs=" + this.carbs + "]";
+    }
+
+    /** Use this style of builder to improve telescoping parameters on constructor */
+    public static class NutritionFactsBuilder {
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbs = 0;
+
+        public NutritionFactsBuilder(final int servingSize, final int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public NutritionFactsBuilder calories(final int val) {
+            this.calories = val;
+            return this;
+        }
+
+        public NutritionFactsBuilder fat(final int val) {
+            this.fat = val;
+            return this;
+        }
+
+        public NutritionFactsBuilder sodium(final int val) {
+            this.sodium = val;
+            return this;
+        }
+
+        public NutritionFactsBuilder carbs(final int val) {
+            this.carbs = val;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(final NutritionFactsBuilder builder) {
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbs = builder.carbs;
+    }
 }
 
 public class Builder_Bloch {
-	public static void main(String... args) {
-		NutritionFactsBuilder builder = new NutritionFactsBuilder(3, 4).calories(5).fat(6);
-		NutritionFacts facts = builder.build();
-		System.out.println(facts);
-	}
+    public static void main(final String... args) {
+        NutritionFacts facts = new NutritionFactsBuilder(3, 4).calories(5).fat(6).build();
+        System.out.println(facts);
+
+        NutritionFacts facts2 = new NutritionFactsBuilder(3, 4).fat(600).build();
+        System.out.println(facts2);
+    }
 }
